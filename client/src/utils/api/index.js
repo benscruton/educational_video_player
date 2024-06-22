0539725742d2fd63a6b7cb3a9d215bd903eb7844
benscruton <benjamin.scruton@gmail.com>
2024-06-22 01:22:58 -0500

    Update index to reflect deleted file

--- client/src/utils/api/index.js
+++ client/src/utils/api/index.js
@@ -5,15 +5,11 @@ import getSingleVideo from "./getSingleVideo";
 import getUserVideos from "./getUserVideos";
 import getVideoComments from "./getVideoComments";
 
-import generateId from "./generateId";
-
 export {
   createComment,
   createVideo,
   editVideo,
   getSingleVideo,
   getUserVideos,
-  getVideoComments,
-
-  generateId
+  getVideoComments
 }
